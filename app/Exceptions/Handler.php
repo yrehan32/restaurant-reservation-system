bdcec9a8040585c7021161042bf8a566a0a10a76
yrehan32 <spada.genk02@gmail.com>
2024-01-25 20:27:45 +0700

    refactor(auth): change exception response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\UnauthorizedException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -23,6 +26,34 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
+        $this->renderable(function (AuthenticationException $e, $request) {
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'error' => true,
+                    'message' => $e->getMessage(),
+                    'data' => null,
+                ], 401);
+            } else {
+                return redirect()->route('login')->with('message', 'You must login first!');
+            }
+        });
+
+        $this->renderable(function (UnauthorizedException $e, $request) {
+            return redirect()->route('login')->with('message', 'You must login first!');
+        });
+
+        $this->renderable(function (AccessDeniedHttpException $e, $request) {
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'error' => true,
+                    'message' => $e->getMessage(),
+                    'data' => null,
+                ], $e->getStatusCode());
+            } else {
+                return abort($e->getStatusCode(), $e->getMessage());
+            }
+        });
+
         $this->reportable(function (Throwable $e) {
             //
         });
